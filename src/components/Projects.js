c6cf13c026aa8cca1e43693608dc2551603afa27
augustaquilon <august.aquilon@gmail.com>
2023-09-06 14:34:51 +0200

    copy pasting from old project

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Projects = () => {
+    return(
+        <section class="featured-container">
+        <section class="featured-content">
+          <h5>
+            Featured Projects
+          </h5>
+          <div class="project-wrapper" id="projectWrapper"></div>
+        </section>
+      </section>
+    )
+}
+
+export default Projects;
