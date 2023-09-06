16dc10be2f9c9005ea2f8183935ce4039612f287
augustaquilon <august.aquilon@gmail.com>
2023-09-06 14:47:56 +0200

    copy pasting information from old portfolio

---
+++ src/components/ProjectList.js
@@ -0,0 +1,44 @@
+export const projectList = [
+    {
+    title: "Project Labyrinth",
+    projectSummary: "It's a labyrinth. It's text-based. Careful, or you might get lost!",
+    tech: "React Redux",
+    live: "https://zippy-vacherin-041127.netlify.app/",
+    code: "https://github.com/TessAquilon/project-labyrinth"
+    },
+    {
+    title: "Project Movies",
+    projectSummary: "Want to know what movies have been released recently? Well, this website displays the latest 20!",
+    tech: "React React Router API:s CSS",
+    live: "https://heartfelt-torrone-e608c2.netlify.app/",
+    code: "https://github.com/TessAquilon/project-movies"
+        },
+    {
+    title: "Project Survey",
+    projectSummary: "Ever played a game of MADLIBS? Here is my own version of it. Check it out!",
+    tech: "React",
+    live: "https://fanciful-capybara-de9924.netlify.app/",
+    code: "https://github.com/TessAquilon/project-survey"
+        },
+    {
+    title: "Project To-do",
+    projectSummary: "It's a to-do list with a cute kitty next to it. A must-have for cat lovers.",
+    tech: "React Redux",
+    live: "https://gregarious-griffin-0ee914.netlify.app/",
+    code: "https://github.com/TessAquilon/project-todos"
+        },
+    {
+    title: "Project Weather App",
+    projectSummary: "Everything you need to know about the weather for the next 5 days.",
+    tech: "HTML CSS JavaScript API:s",
+    live: "https://hippos-weather.netlify.app/",
+    code: "https://github.com/EmmaEngvall/project-weather-app"
+        },
+    {
+    title: "Project Music Releases",
+    projectSummary: "Here you can find the latest album and single releases on Spotify, gathered all in one place.",
+    tech: "React API:s",
+    live: "https://t-and-t-music-release-app.netlify.app/",
+    code: "https://github.com/theresBL/project-music-releases"
+        }
+]
